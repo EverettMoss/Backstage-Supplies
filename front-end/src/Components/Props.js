113163b91196f408aa60925ccb778248b5ec3ab1
everett <edmoss@loyola.edu>
2022-04-28 16:07:24 -0400

    prop page is much better, added modal

--- front-end/src/Components/Props.js
+++ front-end/src/Components/Props.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Axios from "axios";
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table, Button, Modal, Form } from 'react-bootstrap'
 
-function Props(){
+function Props() {
     const [listOfProps, setlistOfProps] = useState([]);
     const [name, setName] = useState("");
     const [material, setMaterial] = useState("");
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     useEffect(() => {
         Axios.get("http://localhost:8000/getProps").then((response) => {
@@ -20,59 +26,92 @@ function Props(){
             material
         }).then((response) => {
             setlistOfProps([
-                ...listOfProps, 
-                {name:name},
-                {material:material}
+                ...listOfProps,
+                { name: name },
+                { material: material }
             ])
         });
 
     };
 
-    return(
+    return (
         <div>
             <h1>Welcome to Props Page!</h1>
-
             <div>
-                <input type="text" placeholder="Enter Name" onChange={(event) =>  { 
-                    setName(event.target.value);
-                }}/>
-                <input type="text" placeholder="Enter Material" onChange={(event) =>  { 
-                    setMaterial(event.target.value);
-                }}/>
-                <button onClick={createProp}>Create Prop</button>
+                <Button variant="outline-secondary" onClick={handleShow}>
+                    Create Prop
+                </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>New Prop</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder ="name"
+                                    onChange={(event) => {setName(event.target.value);}}
+                                    autoFocus
+                                />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Material</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder ="material"
+                                    onChange={(event) => {setMaterial(event.target.value);}}
+                                    autoFocus
+                                />
+                            </Form.Group>
+                    
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={createProp}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
             </div>
-            
+
             <div className="PropsDisplay">
-            <table className="table table-striped">
-                <thead className="thead-light">
-                <tr>
-                    <th>Name</th>
-                    <th>Material</th>
-                    <th>Picture</th>
-                    <th>Location</th>
-                    <th>Use Log</th>
-                    <th>Mending Log</th>
-                    <th>To-Do</th>
-                    <th>Notes</th>
-                </tr>
-                </thead>
-                
-                {
-                    listOfProps.map((prop) => {
-                        return(
-                            <>
-                            <tr>
-                                <td>{prop.name}</td>
-                                <td>{prop.material}</td>
-                            </tr>
-                            </>
-
-
-                        );
-                    })
-                }
-
-            </table>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Material</th>
+                            <th>Picture</th>
+                            <th>Location</th>
+                            <th>Use Log</th>
+                            <th>Mending Log</th>
+                            <th>To-Do</th>
+                            <th>Notes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            listOfProps.map((prop) => {
+                                return (
+                                    <>
+                                        <tr>
+                                            <td>{prop.name}</td>
+                                            <td>{prop.material}</td>
+                                        </tr>
+                                    </>
+                                );
+                            })
+                        }
+                    </tbody>
+                </Table>
             </div>
         </div>
     )
